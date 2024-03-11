b25e41bffbfb8c80fa70888a9e5e65f319d3ebf3
Khaliddxx <doodz@aucegypt.edu>
2024-03-12 02:33:16 +0800

    admin auth

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ dotenv.config();
 // const PORT = 5001;
 
 const DEFAULT_ADMIN = {
-  email: "admin@amit.com",
-  password: "asdf",
+  email: "admin@amitcoders.org",
+  password: "adminSecret@1",
 };
 
 AdminJS.registerAdapter({
@@ -94,40 +94,40 @@ const start = async () => {
         resource: NextCohort,
       },
     ],
-    // auth: {
-    //   authenticate: async (email, password) => {
-    //     // Add your authentication logic here, for example:
-    //     if (email === "admin@example.com" && password === "password") {
-    //       return { email }; // Returning the user object
-    //     } else {
-    //       return null; // Authentication failed
-    //     }
-    //   },
-    // },
+    auth: {
+      authenticate: async (email, password) => {
+        // Add your authentication logic here, for example:
+        if (email === "admin@example.com" && password === "password") {
+          return { email }; // Returning the user object
+        } else {
+          return null; // Authentication failed
+        }
+      },
+    },
   });
 
-  // const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
-  //   admin,
-  //   {
-  //     authenticate,
-  //     cookieName: "adminjs",
-  //     cookiePassword: "sessionsecret",
-  //   },
-  //   null,
-  //   {
-  //     // store: mongooseDb,
-  //     resave: true,
-  //     saveUninitialized: true,
-  //     secret: "sessionsecret",
-  //     cookie: {
-  //       httpOnly: process.env.NODE_ENV === "production",
-  //       secure: process.env.NODE_ENV === "production",
-  //     },
-  //     name: "adminjs",
-  //   }
-  // );
+  const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    admin,
+    {
+      authenticate,
+      cookieName: "adminjs",
+      cookiePassword: "sessionsecret",
+    },
+    null,
+    {
+      // store: mongooseDb,
+      resave: true,
+      saveUninitialized: true,
+      secret: "sessionsecret",
+      cookie: {
+        httpOnly: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production",
+      },
+      name: "adminjs",
+    }
+  );
 
-  const adminRouter = AdminJSExpress.buildRouter(admin);
+  // const adminRouter = AdminJSExpress.buildRouter(admin);
   app.use(admin.options.rootPath, adminRouter);
 
   app.listen(process.env.PORT || "3000", () => {
